6008dc7d0aa36bd05a895bd695d77f0798bf6455
JOSE LUIS VELAZQUEZ <joseluisvelazquezro@gmail.com>
2024-07-31 15:18:03 -0600

    perfecto

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,14 +1,14 @@
 #Prompt
 Import-Module posh-git
-Import-Module oh-my-posh
-Set-PoshPrompt Paradox
+#Import-Module oh-my-posh
+#Get-PoshThemes Paradox
 #Set-PoshPrompt mojada
 #Set-PoshPrompt iterm2 
 #Set-PoshPrompt powerlevel10k_rainbow
 
 #Load prompt config
 function Get-ScriptDirectory { Split-Path $MyInvocation.ScriptName }
-$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'velar.omp.json'
+$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'powerlevel10k_rainbow.omp.json'
 oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
 
 #Icons
